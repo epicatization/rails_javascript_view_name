ec168a1aff723e357162197a842d412a28b87fe6
Radosław Snarski <radeksnara@gmail.com>
2017-01-14 10:19:55 +0100

    allow to set js views to partials

--- java_script_view_name.py
+++ java_script_view_name.py
@@ -2,14 +2,16 @@ import sublime, sublime_plugin, os
 
 class JavaScriptViewNameCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-      filename = os.path.dirname(self.view.file_name())
+      filename = self.view.file_name().split('.')[0]
       if 'views' in filename:
         path = filename.split('views/')[1]
         create_views = [x.title() for x in path.split('/')]
         filename = "Views." + '.'.join(create_views)
         filename = filename.replace('_','')
+
       sublime.set_clipboard(filename)
       sublime.status_message("Copied file name: %s" % filename)
-
+      # /app/views/admin/items/form.html.haml
+      # Views.Admin.Items.Form
 
 
